797be98170efd8195d5108ff86ac36ba1a3ca4cb
VersionXV <VersionXV@gmail.com>
2026-07-17 20:27:43 -0300

    first draft

---
+++ scripts/place.js
@@ -0,0 +1,7 @@
+const today = new Date();
+
+const currentYear = today.getFullYear();
+
+document.getElementById('currentyear').textContent = currentYear
+
+document.getElementById('lastmodified').textContent = document.lastModified;
